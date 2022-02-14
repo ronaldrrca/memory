06772e3e215b948d1468c32ee1a2cf336cae00a2
Ronald Rodriguez Campillo <ronaldrrca@gmail.com>
2022-02-14 18:26:42 -0500

    Creando archivos, estructura y estilos básicos

---
+++ code.js
@@ -0,0 +1,183 @@
+const images = [
+    {
+        nameImage: "bear",
+        src: "assets/images/bear.svg",
+        alt: "imagen de oso"
+    },
+    {
+        nameImage: "bear",
+        src: "assets/images/bear.svg",
+        alt: "imagen de oso"
+    },
+    {
+        nameImage: "bear",
+        src: "assets/images/bear.svg",
+        alt: "imagen de oso"
+    },
+    {
+        nameImage: "camel",
+        src: "assets/images/camel.svg",
+        alt: "imagen de camello"
+    },
+    {
+        nameImage: "camel",
+        src: "assets/images/camel.svg",
+        alt: "imagen de camello"
+    },
+    {
+        nameImage: "camel",
+        src: "assets/images/camel.svg",
+        alt: "imagen de camello"
+    },
+    {
+        nameImage: "dolphin",
+        src: "assets/images/dolphin.svg",
+        alt: "imagen de delfin"
+    },
+    {
+        nameImage: "dolphin",
+        src: "assets/images/dolphin.svg",
+        alt: "imagen de delfin"
+    },
+    {
+        nameImage: "dolphin",
+        src: "assets/images/dolphin.svg",
+        alt: "imagen de delfin"
+    },
+    {
+        nameImage: "elephant",
+        src: "assets/images/elephant.svg",
+        alt: "imagen de elefante"
+    },
+    {
+        nameImage: "elephant",
+        src: "assets/images/elephant.svg",
+        alt: "imagen de elefante"
+    },
+    {
+        nameImage: "elephant",
+        src: "assets/images/elephant.svg",
+        alt: "imagen de elefante"
+    },
+    {
+        nameImage: "gorilla",
+        src: "assets/images/gorilla.svg",
+        alt: "imagen de gorila"
+    },
+    {
+        nameImage: "gorilla",
+        src: "assets/images/gorilla.svg",
+        alt: "imagen de gorila"
+    },
+    {
+        nameImage: "gorilla",
+        src: "assets/images/gorilla.svg",
+        alt: "imagen de gorila"
+    },
+    {
+        nameImage: "hippopotamus",
+        src: "assets/images/hippopotamus.svg",
+        alt: "imagen de hipopotamo"
+    },
+    {
+        nameImage: "hippopotamus",
+        src: "assets/images/hippopotamus.svg",
+        alt: "imagen de hipopotamo"
+    },
+    {
+        nameImage: "hippopotamus",
+        src: "assets/images/hippopotamus.svg",
+        alt: "imagen de hipopotamo"
+    },
+    {
+        nameImage: "kangaroo",
+        src: "assets/images/kangaroo.svg",
+        alt: "imagen de canguro"
+    },
+    {
+        nameImage: "kangaroo",
+        src: "assets/images/kangaroo.svg",
+        alt: "imagen de canguro"
+    },
+    {
+        nameImage: "kangaroo",
+        src: "assets/images/kangaroo.svg",
+        alt: "imagen de canguro"
+    },
+    {
+        nameImage: "koala",
+        src: "assets/images/koala.svg",
+        alt: "imagen de koala"
+    },
+    {
+        nameImage: "koala",
+        src: "assets/images/koala.svg",
+        alt: "imagen de koala"
+    },
+    {
+        nameImage: "koala",
+        src: "assets/images/koala.svg",
+        alt: "imagen de koala"
+    },
+    {
+        nameImage: "lion",
+        src: "assets/images/lion.svg",
+        alt: "imagen de leon"
+    },
+    {
+        nameImage: "lion",
+        src: "assets/images/lion.svg",
+        alt: "imagen de leon"
+    },
+    {
+        nameImage: "lion",
+        src: "assets/images/lion.svg",
+        alt: "imagen de leon"
+    },
+    {
+        nameImage: "rabbit",
+        src: "assets/images/rabbit.svg",
+        alt: "imagen de conejo"
+    },
+    {
+        nameImage: "rabbit",
+        src: "assets/images/rabbit.svg",
+        alt: "imagen de conejo"
+    },
+    {
+        nameImage: "rabbit",
+        src: "assets/images/rabbit.svg",
+        alt: "imagen de conejo"
+    },
+    {
+        nameImage: "rhinoceros",
+        src: "assets/images/rhinoceros.svg",
+        alt: "imagen de rinoceronte"
+    },
+    {
+        nameImage: "rhinoceros",
+        src: "assets/images/rhinoceros.svg",
+        alt: "imagen de rinoceronte"
+    },
+    {
+        nameImage: "rhinoceros",
+        src: "assets/images/rhinoceros.svg",
+        alt: "imagen de rinoceronte"
+    },
+    {
+        nameImage: "zebra",
+        src: "assets/images/zebra.svg",
+        alt: "imagen de zebra"
+    },
+    {
+        nameImage: "zebra",
+        src: "assets/images/zebra.svg",
+        alt: "imagen de zebra"
+    },
+    {
+        nameImage: "zebra",
+        src: "assets/images/zebra.svg",
+        alt: "imagen de zebra"
+    },
+]
+
